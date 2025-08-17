74031378871f7fd3340c3201c6567ced8b693979
ccai <143898675+ChengzhiTsoi@users.noreply.github.com>
2025-08-17 21:57:47 +0800

    Update required_path.sh

--- required_path.sh
+++ required_path.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # === Routes Configuration ===
-#export LAMMPS_INT_DIR="/afs/crc.nd.edu/user/c/ccai/lammps-env"
 export LAMMPS_DIR="/afs/crc.nd.edu/user/c/ccai/lammps/lammps-29Aug2024"
 export RASPA_DIR="/afs/crc.nd.edu/user/c/ccai/RASPA/simulations"
 
